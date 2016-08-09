2ce7ff037fedd1fdbd54b52bfde41f5f276b0d26
Mathias Maes <ikkeboe@outlook.com>
2016-08-09 21:16:47 +0200

    ADD:Enemy AI

---
+++ Game4Fun/Assets/Game/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class EnemyMovement : MonoBehaviour
+{
+
+    public float Speed = 10f;
+    public Transform[] WayPoints;
+
+    private Rigidbody2D rigi;
+    private int wp = 0;
+
+    void Start()
+    {
+        if(WayPoints.Length < 2)
+        {
+            Destroy(gameObject);
+            Debug.LogError("There were not enough waypoints");
+            return;
+        }
+
+        rigi = GetComponent<Rigidbody2D>();
+
+        transform.position = WayPoints[0].position;
+        transform.rotation = WayPoints[0].rotation;
+
+        wp = 1;
+    }
+
+    void Update()
+    {
+        RotateToWaypoint();
+
+        Vector3 dir = WayPoints[wp].position - transform.position;
+        transform.Translate(dir.normalized * Time.deltaTime * Speed, Space.World);
+
+
+        if(Vector3.Distance(transform.position,WayPoints[wp].position) <= 0.2f)
+        {
+            wp++;
+            if (wp == WayPoints.Length) wp = 0;
+        }
+
+    }
+
+    private void RotateToWaypoint()
+    {
+
+    }
+}
