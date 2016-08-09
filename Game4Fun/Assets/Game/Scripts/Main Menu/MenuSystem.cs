eb6d50485447bcbd61601141ff9ad517bb34ae44
WhyAreLess <zimonelmerup@outlook.com>
2016-08-09 21:13:21 +0200

    Main Menu

---
+++ Game4Fun/Assets/Game/Scripts/Main Menu/MenuSystem.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSystem : MonoBehaviour {
+
+	public Transform menuCanvas;
+	
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			Paused();
+		}
+	}
+
+	public void Paused()
+	{
+		if(menuCanvas.gameObject.activeInHierarchy == false)
+		{
+			menuCanvas.gameObject.SetActive(true);
+			Time.timeScale = 0;
+		}
+		else
+		{
+			menuCanvas.gameObject.SetActive(false);
+			Time.timeScale = 1;
+		}
+	}
+
+	public void Exit()
+	{
+		Application.Quit();
+	}
+}
