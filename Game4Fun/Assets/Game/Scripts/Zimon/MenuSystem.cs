ca4ca4b0cc2b3ea43274d3852642d3d83c6679ac
WhyAreLess <zimonelmerup@outlook.com>
2016-08-10 15:31:55 +0200

    Cursor & Main Menu

---
+++ Game4Fun/Assets/Game/Scripts/Zimon/MenuSystem.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSystem : MonoBehaviour {
+
+	public Transform menuCanvas;
+
+	void Start () 
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+	
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			PausedGame();
+		}
+	}
+
+	public void PausedGame()
+	{
+		if(menuCanvas.gameObject.activeInHierarchy == false)
+			{
+				menuCanvas.gameObject.SetActive(true);
+				Cursor.lockState = CursorLockMode.None;
+				Cursor.visible = true;
+				Time.timeScale = 0;
+			}
+			else
+			{
+				menuCanvas.gameObject.SetActive(false);
+				Cursor.lockState = CursorLockMode.Locked;
+				Cursor.visible = false;
+				Time.timeScale = 1;
+			}
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
