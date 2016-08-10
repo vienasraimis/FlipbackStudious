e504877a172f7e20aba33697587cfc2c91a558ae
WhyAreLess <zimonelmerup@outlook.com>
2016-08-10 20:43:48 +0200

    PortalSwitchScene Script - Easy to add more levels now

---
+++ Game4Fun/Assets/Game/Scripts/Zimon/PortalSwitchScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PortalSwitchScene : MonoBehaviour {
+
+	[SerializeField]
+	string levelToLoad;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			SceneManager.LoadScene(levelToLoad);
+		}
+	}
+}
