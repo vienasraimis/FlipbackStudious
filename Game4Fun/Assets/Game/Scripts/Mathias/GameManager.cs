00b3a0d43c2783ffd1ae73d62a8bb886fbef3129
Mathias Maes <ikkeboe@outlook.com>
2016-08-10 14:17:02 +0200

    Added GameManager Script

---
+++ Game4Fun/Assets/Game/Scripts/Mathias/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public float volume = 1f;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if(PlayerPrefs.GetInt("FirstRun") == 0)
+        {
+            PlayerPrefs.SetInt("FirstRun", 1);
+            PlayerPrefs.SetFloat("Volume", 1f);
+
+            PlayerPrefs.Save();
+        }
+
+        volume = PlayerPrefs.GetFloat("Volume");
+        ChangeVolume();
+
+        SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+    }
+
+    private void SceneManager_sceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        ChangeVolume();
+    }
+
+
+    public void ChangeVolume()
+    {
+
+        var allAudioSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+        foreach (var au in allAudioSources)
+        {
+            au.volume = volume;
+        }
+    }
+
+}
