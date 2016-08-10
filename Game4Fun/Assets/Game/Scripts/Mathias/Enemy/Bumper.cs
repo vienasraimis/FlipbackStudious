2d98416c7b4aa3573aa51ed3396fbb8cb2f43839
Mathias Maes <ikkeboe@outlook.com>
2016-08-10 15:50:47 +0200

    Start Bumper

---
+++ Game4Fun/Assets/Game/Scripts/Mathias/Enemy/Bumper.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bumper : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.transform.tag == "Player")
+        {
+
+        }
+    }
+}
