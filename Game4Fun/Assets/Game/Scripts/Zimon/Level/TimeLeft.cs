dbc2ee2110bb02bcba39c37f6457f4fd2a1244c3
WhyAreLess <zimonelmerup@outlook.com>
2016-08-11 06:51:04 +0200

    All levels now have a TimeLeft script, making it more challenging

---
+++ Game4Fun/Assets/Game/Scripts/Zimon/Level/TimeLeft.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TimeLeft : MonoBehaviour {
+
+	[SerializeField]
+	string levelToLoad;
+
+	public float time;
+
+	void FixedUpdate()
+	{
+		time -= Time.fixedDeltaTime;
+
+		if(time <= 0f)
+		{
+			SceneManager.LoadScene(levelToLoad);
+		}
+	}
+}
