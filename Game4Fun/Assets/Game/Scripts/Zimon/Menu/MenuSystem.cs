0c9d607b22d35d4ec98e2e70b81691861b59cdcd
whyareless <zimonelmerup@outlook.com>
2016-08-12 17:44:08 +0200

    added level select and credits menu to main menu, changed main menu design

--- Game4Fun/Assets/Game/Scripts/Zimon/Menu/MenuSystem.cs
+++ Game4Fun/Assets/Game/Scripts/Zimon/Menu/MenuSystem.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class MenuSystem : MonoBehaviour {
 
 	public Transform menuCanvas;
+	public Transform mainMenu;
+	public Transform levelSelectMenu;
+	public Transform creditsMenu;
 
 	void Start () 
 	{
@@ -23,18 +26,53 @@ public class MenuSystem : MonoBehaviour {
 	{
 		if(menuCanvas.gameObject.activeInHierarchy == false)
 			{
+				//Active
 				menuCanvas.gameObject.SetActive(true);
+				mainMenu.gameObject.SetActive(true);
 				Cursor.lockState = CursorLockMode.None;
 				Cursor.visible = true;
 				Time.timeScale = 0;
+
+				//Inactive
+				levelSelectMenu.gameObject.SetActive(false);
+				creditsMenu.gameObject.SetActive(false);
 			}
 			else
 			{
 				menuCanvas.gameObject.SetActive(false);
+				mainMenu.gameObject.SetActive(false);
 				Cursor.lockState = CursorLockMode.Locked;
 				Cursor.visible = false;
 				Time.timeScale = 1;
 			}
+	}
+
+	public void LevelSelect(bool OpenMenu)
+	{
+		if(OpenMenu)
+		{
+			levelSelectMenu.gameObject.SetActive(true);
+			mainMenu.gameObject.SetActive(false);
+		}
+		if(!OpenMenu)
+		{
+			levelSelectMenu.gameObject.SetActive(false);
+			mainMenu.gameObject.SetActive(true);
+		}
+	}
+
+	public void Credits(bool OpenMenu)
+	{
+		if(OpenMenu)
+		{
+			creditsMenu.gameObject.SetActive(true);
+			mainMenu.gameObject.SetActive(false);
+		}
+		if(!OpenMenu)
+		{
+			creditsMenu.gameObject.SetActive(false);
+			mainMenu.gameObject.SetActive(true);
+		}
 	}
 
 	public void ExitGame()
