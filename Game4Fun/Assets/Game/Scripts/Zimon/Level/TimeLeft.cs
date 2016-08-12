a210db87f8a72aff7dc3ae518024baa744395770
Mathias Maes <ikkeboe@outlook.com>
2016-08-12 11:42:37 +0200

    Fixed timescript Added timebar

--- Game4Fun/Assets/Game/Scripts/Zimon/Level/TimeLeft.cs
+++ Game4Fun/Assets/Game/Scripts/Zimon/Level/TimeLeft.cs
@@ -1,21 +1,34 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class TimeLeft : MonoBehaviour {
 
 	[SerializeField]
-	string levelToLoad;
+	private string levelToLoad;
+
+    [SerializeField]
+    private Slider TimeBar;
 
 	public float time;
+
+    void Start()
+    {
+        TimeBar.maxValue = time;
+        TimeBar.value = time;
+    }
 
 	void FixedUpdate()
 	{
 		time -= Time.fixedDeltaTime;
+        TimeBar.value = time;
+
 
 		if(time <= 0f)
 		{
-			//SceneManager.LoadScene(levelToLoad);
-		}
+            var over = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameOver>();
+            over.Gameover();
+        }
 	}
 }
